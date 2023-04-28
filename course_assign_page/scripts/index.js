35f27701434f7eeae1462813cb30f20b51c920be
Use your GitHub username <Enter your email address>
2023-04-28 14:55:06 -0500

    java

--- course_assign_page/scripts/index.js
+++ course_assign_page/scripts/index.js
@@ -1,5 +1,20 @@
 // This function is to update the date and time for the last
 // time the site was updated.
-var lastUpdated = document.querySelector('datetime');
-let oLastModif = new Date(document.lastModified);    
-// lastUpdated.textContent = 'Last updated: ' + oLastModif; 
+// let lastUpdated = document.querySelector('datetime');
+// let LastModif = new Date(document.lastModified);    
+// lastUpdated.textContent = 'Last updated: ' + LastModif; 
+
+// Create a new Date object
+var currentDate = new Date();
+
+// Get the date
+var date = currentDate.toDateString();
+
+// Get the time
+var time = currentDate.toLocaleTimeString();
+
+// Use querySelector to select the HTML element
+var datetimeElement = document.querySelector('#datetime');
+
+// Set the content of the HTML element to the date and time
+datetimeElement.textContent = 'Last Updated: ' + date + time;
