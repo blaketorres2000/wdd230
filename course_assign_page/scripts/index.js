fa4f243f8eea45be6390b9eaf965303405a8e750
Use your GitHub username <Enter your email address>
2023-04-28 11:02:35 -0500

    javascript

--- course_assign_page/scripts/index.js
+++ course_assign_page/scripts/index.js
@@ -1,8 +1,18 @@
-function dateTimeUpdated () {
-    let dateTimeElement = document.querySelector('dateTime');
-    let currentDate = new Date();
-    let dateTimeString = currentDate.toLocaleString();
-    dateTimeElement.textContent = dateTimeString;
+// function dateTimeUpdated () {
+//     let dateTimeElement = document.querySelector('dateTime');
+//     let currentDate = new Date();
+//     let dateTimeString = currentDate.toLocaleString();
+//     dateTimeElement.textContent = dateTimeString;
+// }
+
+// setInterval(dateTimeUpdated,1000);
+
+function updateDateTime() {
+    var datetimeElement = document.getElementById("datetime");
+    var currentDate = new Date();
+    var dateTimeString = currentDate.toLocaleString();
+    datetimeElement.textContent = dateTimeString;
 }
 
-setInterval(dateTimeUpdated,1000);
+// Update the date and time every second (1000 milliseconds)
+setInterval(updateDateTime, 1000);
