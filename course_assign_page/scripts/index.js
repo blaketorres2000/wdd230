993576a6dec6cb7a6a0285a62d0c8ed593b0a0e8
Use your GitHub username <Enter your email address>
2023-04-28 17:02:30 -0500

    final corrections

--- course_assign_page/scripts/index.js
+++ course_assign_page/scripts/index.js
@@ -1,9 +1,6 @@
-// This function is to update the date and time for the last
-// time the site was updated.
-// let lastUpdated = document.querySelector('datetime');
-// let LastModif = new Date(document.lastModified);    
-// lastUpdated.textContent = 'Last updated: ' + LastModif; 
 
+// This function is to display the copyright symbol,
+// current date, name, and state.
 function copyNameState() {
     const copyrightSymbol = '\u00A9';
     let currentDate = new Date();
@@ -12,20 +9,13 @@ function copyNameState() {
     year.textContent = copyrightSymbol + '  ' + currentYear + '  .:| Blake K. Torres |:. Texas';
 }
 
+// This function is to update the date and time for the last
+// time the site was updated.
 function updateDateTime() {
-    // Create a new Date object
     let currentDate = new Date();
-
-    // Get the date
     let date = currentDate.toDateString();
-
-    // Get the time
     let time = currentDate.toLocaleTimeString();
-
-    // Use querySelector to select the HTML element
     let datetimeElement = document.querySelector('#datetime');
-
-    // Set the content of the HTML element to the date and time
     datetimeElement.textContent = 'Last Updated: ' + date + '   ' + time;
 }
 
