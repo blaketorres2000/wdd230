16f11615549a7457735ee12ec6c080f6827de2bc
Use your GitHub username <Enter your email address>
2023-05-24 19:54:56 -0500

    added if statement to stop user from adding blank

--- lesson5/scripts/bom.js
+++ lesson5/scripts/bom.js
@@ -6,19 +6,21 @@ button.addEventListener("click", () => {
     const myItem = input.value;
     input.value = "";
 
-    const listItem = document.createElement("li");
-    const listText = document.createElement("span");
-    const listButton = document.createElement("button");
+    if (myItem !== "") { // Check if myItem is not empty
+        const listItem = document.createElement("li");
+        const listText = document.createElement("span");
+        const listButton = document.createElement("button");
 
-    listItem.appendChild(listText);
-    listText.textContent = myItem;
-    listItem.appendChild(listButton);
-    listButton.textContent = "X";
-    listButton.style.color = "red";
-    list.appendChild(listItem);
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listButton);
+        listButton.textContent = "X";
+        listButton.style.color = "red";
+        list.appendChild(listItem);
 
-    listButton.addEventListener("click", () => {
-        list.removeChild(listItem);
-    });
+        listButton.addEventListener("click", () => {
+            list.removeChild(listItem);
+        });
+    }
     input.focus();
 });
