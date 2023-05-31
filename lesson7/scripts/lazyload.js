9350d74056802f3412dba9b6353d55a1e96ec3cd
Use your GitHub username <Enter your email address>
2023-05-31 17:31:25 -0500

    Week 7 corrections

--- lesson7/scripts/lazyload.js
+++ lesson7/scripts/lazyload.js
@@ -1,17 +1,17 @@
-const placeholderImages = document.querySelectorAll('.placeholder');
+const placeholderImages = document.querySelectorAll(".placeholder");
 
 function loadImage(entry) {
   const imgElement = entry.target;
   const newImage = new Image();
-  newImage.src = imgElement.getAttribute('data-src');
+  newImage.src = imgElement.getAttribute("data-src");
 
-  newImage.addEventListener('load', function() {
+  newImage.addEventListener("load", function () {
     imgElement.src = newImage.src;
   });
 }
 
-const observer = new IntersectionObserver(function(entries, observer) {
-  entries.forEach(function(entry) {
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach(function (entry) {
     if (entry.isIntersecting) {
       loadImage(entry);
       observer.unobserve(entry.target);
@@ -19,6 +19,6 @@ const observer = new IntersectionObserver(function(entries, observer) {
   });
 });
 
-placeholderImages.forEach(function(image) {
+placeholderImages.forEach(function (image) {
   observer.observe(image);
 });
