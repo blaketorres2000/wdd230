11a6194032f475dc9e5d1b23994fe177a0ff6055
Use your GitHub username <Enter your email address>
2023-06-29 12:05:57 -0500

    directory changeable from card to list view

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,63 +1,145 @@
-
-const datajson = 'json/data.json';
+const datajson = "json/data.json";
+let isCardView = true; // Flag to track the current view mode
 
 async function getBusinessData() {
-    const response = await fetch(datajson);
-    const data = await response.json();
-    displayBusinesses(data.businesses);
+  const response = await fetch(datajson);
+  const data = await response.json();
+  displayBusinesses(data.businesses);
 }
 
+const toggleView = () => {
+  const cards = document.querySelector("div.cards");
+  const toggleButton = document.getElementById("toggle-button");
+
+  // Toggle the flag and update the view
+  isCardView = !isCardView;
+  toggleButton.textContent = isCardView ? "List View" : "Card View";
+
+  if (isCardView) {
+    cards.classList.remove("list-view");
+  } else {
+    cards.classList.add("list-view");
+  }
+};
+
 const displayBusinesses = (businesses) => {
-    const cards = document.querySelector('div.cards');
+  const cards = document.querySelector("div.cards");
 
-    businesses.forEach((business) => {
-        let card = document.createElement('section');
-        card.setAttribute('class', 'business-card');
+  businesses.forEach((business) => {
+    let card = document.createElement("section");
+    card.setAttribute("class", "business-card");
 
-        let h2 = document.createElement('h2');
-        h2.setAttribute('id', 'business-name');
+    let h2 = document.createElement("h2");
+    h2.setAttribute("id", "business-name");
+    h2.textContent = `${business.name}`;
 
-        let p = document.createElement('p');
-        p.setAttribute('class', 'business-contact');
+    let div1 = document.createElement("div");
+    div1.setAttribute("class", "contact-container");
+    let p = document.createElement("p");
+    p.setAttribute("class", "business-contact");
+    p.textContent = `Contact: ${business.contact}`;
+    let p2 = document.createElement("p");
+    p2.setAttribute("class", "business-established");
+    p2.textContent = `Established: ${business.established}`;
+    div1.appendChild(p);
+    div1.appendChild(p2);
 
-        let p2 = document.createElement('p');
-        p2.setAttribute('class', 'business-established');
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "address-container");
+    let p3 = document.createElement("p");
+    p3.setAttribute("class", "business-address");
+    p3.textContent = `Address: ${business.address}`;
+    let p4 = document.createElement("p");
+    p4.setAttribute("class", "business-phone");
+    p4.textContent = `Phone: ${business.phone}`;
+    let p5 = document.createElement("p");
+    p5.setAttribute("class", "business-website");
+    p5.textContent = `${business.url}`;
+    div2.appendChild(p3);
+    div2.appendChild(p4);
+    div2.appendChild(p5);
 
-        let p3 = document.createElement('p');
-        p3.setAttribute('class', 'business-address');
+    let portrait = document.createElement("img");
+    portrait.setAttribute("id", "business-image");
+    portrait.setAttribute("src", business.image);
+    portrait.setAttribute("alt", `Portrait of ${business.name}`);
+    portrait.setAttribute("loading", "lazy");
+    portrait.setAttribute("width", "340");
+    portrait.setAttribute("height", "440");
 
-        let p4 = document.createElement('p');
-        p4.setAttribute('class', 'business-phone');
+    card.appendChild(h2);
+    card.appendChild(div1);
+    card.appendChild(div2);
+    card.appendChild(portrait);
 
-        let p5 = document.createElement('p');
-        p5.setAttribute('class', 'business-website');
-
-        let portrait = document.createElement('img');
-        portrait.setAttribute('id', 'business-image');
-
-        h2.textContent = `${business.name}`;
-        p.textContent = `Contact: ${business.contact}`;
-        p2.textContent = `Established: ${business.established}`;
-        p3.textContent = `Address: ${business.address}`;
-        p4.textContent = `Phone: ${business.phone}`;
-        p5.textContent = `Website: ${business.url}`;
-
-        portrait.setAttribute('src', business.image);
-        portrait.setAttribute('alt', `Portrait of ${business.name}`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
-
-        card.appendChild(h2);
-        card.appendChild(p);
-        card.appendChild(p2);
-        card.appendChild(p3);
-        card.appendChild(p4);
-        card.appendChild(p5);
-        card.appendChild(portrait);
-
-        cards.appendChild(card);
-    });
+    cards.appendChild(card);
+  });
 };
 
 getBusinessData();
+
+const toggleButton = document.querySelector("#toggle-button");
+toggleButton.addEventListener("click", toggleView);
+
+// const datajson = 'json/data.json';
+
+// async function getBusinessData() {
+//     const response = await fetch(datajson);
+//     const data = await response.json();
+//     displayBusinesses(data.businesses);
+// }
+
+// const displayBusinesses = (businesses) => {
+//     const cards = document.querySelector('div.cards');
+
+//     businesses.forEach((business) => {
+//         let card = document.createElement('section');
+//         card.setAttribute('class', 'business-card');
+
+//         let h2 = document.createElement('h2');
+//         h2.setAttribute('id', 'business-name');
+
+//         let p = document.createElement('p');
+//         p.setAttribute('class', 'business-contact');
+
+//         let p2 = document.createElement('p');
+//         p2.setAttribute('class', 'business-established');
+
+//         let p3 = document.createElement('p');
+//         p3.setAttribute('class', 'business-address');
+
+//         let p4 = document.createElement('p');
+//         p4.setAttribute('class', 'business-phone');
+
+//         let p5 = document.createElement('p');
+//         p5.setAttribute('class', 'business-website');
+
+//         let portrait = document.createElement('img');
+//         portrait.setAttribute('id', 'business-image');
+
+//         h2.textContent = `${business.name}`;
+//         p.textContent = `Contact: ${business.contact}`;
+//         p2.textContent = `Established: ${business.established}`;
+//         p3.textContent = `Address: ${business.address}`;
+//         p4.textContent = `Phone: ${business.phone}`;
+//         p5.textContent = `Website: ${business.url}`;
+
+//         portrait.setAttribute('src', business.image);
+//         portrait.setAttribute('alt', `Portrait of ${business.name}`);
+//         portrait.setAttribute('loading', 'lazy');
+//         portrait.setAttribute('width', '340');
+//         portrait.setAttribute('height', '440');
+
+//         card.appendChild(h2);
+//         card.appendChild(p);
+//         card.appendChild(p2);
+//         card.appendChild(p3);
+//         card.appendChild(p4);
+//         card.appendChild(p5);
+//         card.appendChild(portrait);
+
+//         cards.appendChild(card);
+//     });
+// };
+
+// getBusinessData();
