c06cf59ee1040fdb89b03fe5c0b342ddb7fe9952
Use your GitHub username <Enter your email address>
2023-07-01 13:49:45 -0500

    more changes to the final site

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -2,30 +2,30 @@ const datajson = "json/data.json";
 let isCardView = true; // Flag to track the current view mode
 
 async function getBusinessData() {
-  const response = await fetch(datajson);
-  const data = await response.json();
-  displayBusinesses(data.businesses);
+const response = await fetch(datajson);
+const data = await response.json();
+displayBusinesses(data.businesses);
 }
 
 const toggleView = () => {
-  const cards = document.querySelector("div.cards");
-  const toggleButton = document.getElementById("toggle-button");
+const cards = document.querySelector("div.cards");
+const toggleButton = document.getElementById("toggle-button");
 
   // Toggle the flag and update the view
-  isCardView = !isCardView;
-  toggleButton.textContent = isCardView ? "List View" : "Card View";
+isCardView = !isCardView;
+toggleButton.textContent = isCardView ? "List View" : "Card View";
 
-  if (isCardView) {
+if (isCardView) {
     cards.classList.remove("list-view");
-  } else {
+} else {
     cards.classList.add("list-view");
-  }
+}
 };
 
 const displayBusinesses = (businesses) => {
-  const cards = document.querySelector("div.cards");
+const cards = document.querySelector("div.cards");
 
-  businesses.forEach((business) => {
+businesses.forEach((business) => {
     let card = document.createElement("section");
     card.setAttribute("class", "business-card");
 
@@ -73,73 +73,10 @@ const displayBusinesses = (businesses) => {
     card.appendChild(portrait);
 
     cards.appendChild(card);
-  });
+});
 };
 
 getBusinessData();
 
 const toggleButton = document.querySelector("#toggle-button");
 toggleButton.addEventListener("click", toggleView);
-
-// const datajson = 'json/data.json';
-
-// async function getBusinessData() {
-//     const response = await fetch(datajson);
-//     const data = await response.json();
-//     displayBusinesses(data.businesses);
-// }
-
-// const displayBusinesses = (businesses) => {
-//     const cards = document.querySelector('div.cards');
-
-//     businesses.forEach((business) => {
-//         let card = document.createElement('section');
-//         card.setAttribute('class', 'business-card');
-
-//         let h2 = document.createElement('h2');
-//         h2.setAttribute('id', 'business-name');
-
-//         let p = document.createElement('p');
-//         p.setAttribute('class', 'business-contact');
-
-//         let p2 = document.createElement('p');
-//         p2.setAttribute('class', 'business-established');
-
-//         let p3 = document.createElement('p');
-//         p3.setAttribute('class', 'business-address');
-
-//         let p4 = document.createElement('p');
-//         p4.setAttribute('class', 'business-phone');
-
-//         let p5 = document.createElement('p');
-//         p5.setAttribute('class', 'business-website');
-
-//         let portrait = document.createElement('img');
-//         portrait.setAttribute('id', 'business-image');
-
-//         h2.textContent = `${business.name}`;
-//         p.textContent = `Contact: ${business.contact}`;
-//         p2.textContent = `Established: ${business.established}`;
-//         p3.textContent = `Address: ${business.address}`;
-//         p4.textContent = `Phone: ${business.phone}`;
-//         p5.textContent = `Website: ${business.url}`;
-
-//         portrait.setAttribute('src', business.image);
-//         portrait.setAttribute('alt', `Portrait of ${business.name}`);
-//         portrait.setAttribute('loading', 'lazy');
-//         portrait.setAttribute('width', '340');
-//         portrait.setAttribute('height', '440');
-
-//         card.appendChild(h2);
-//         card.appendChild(p);
-//         card.appendChild(p2);
-//         card.appendChild(p3);
-//         card.appendChild(p4);
-//         card.appendChild(p5);
-//         card.appendChild(portrait);
-
-//         cards.appendChild(card);
-//     });
-// };
-
-// getBusinessData();
